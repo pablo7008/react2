720192ce2129afe863ae845b56ab933311278fd5
Pablo Robledo <pablorobledo@live.com.ar>
2023-10-04 13:02:06 -0300

    2da Entrega Ok

--- src/Components/CompUniversal/Busqueda.jsx
+++ src/Components/CompUniversal/Busqueda.jsx
@@ -1,1 +1,1 @@
-// Cuando aplique uso de FireBase y genere la BD aca pondre la solicitud mediante API para conseguir los datos y usar el componente de forma universal
+// Cuando aplique uso de FireBase y genere la BD aca pondre la solicitud mediante API para conseguir los datos y usar el componente de forma universal..
